f81fbe7bc99e374cd3b682a5c1fe477e898573df
정대현 <eogus_2513@dsm.hs.kr>
2023-04-20 10:23:16 +0900

    feat: Url Redirect 처리

--- src/main/kotlin/com/project/surl/url/controller/UrlController.kt
+++ src/main/kotlin/com/project/surl/url/controller/UrlController.kt
@@ -2,17 +2,17 @@ package com.project.surl.url.controller
 
 import com.project.surl.url.controller.dto.request.GenerateShortUrlRequest
 import com.project.surl.url.controller.dto.response.GetShortUrlResponse
-import com.project.surl.url.controller.dto.response.GetUrlResponse
 import com.project.surl.url.service.UrlService
 import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.result.view.RedirectView
 
-@RestController
+@Controller
 class UrlController(
     private val urlService: UrlService,
 ) {
@@ -24,7 +24,8 @@ class UrlController(
     }
 
     @GetMapping("/{shortUrl}")
-    suspend fun getUrl(@PathVariable shortUrl: String): String {
-        return urlService.getUrl(shortUrl)
+    suspend fun getUrl(@PathVariable shortUrl: String): RedirectView {
+        val url = urlService.getUrl(shortUrl)
+        return RedirectView(url)
     }
 }
