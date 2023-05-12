1b62a07760228d23c91b448164b0bfe140a72238
정대현 <eogus_2513@dsm.hs.kr>
2023-05-12 09:15:20 +0900

    fix: CORS 처리

--- src/main/kotlin/com/project/surl/global/configuration/WebConfig.kt
+++ src/main/kotlin/com/project/surl/global/configuration/WebConfig.kt
@@ -2,9 +2,11 @@ package com.project.surl.global.configuration
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
+@EnableWebFlux
 class WebConfig : WebFluxConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
