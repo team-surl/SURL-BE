dc6638a29e20be68077fb60f0ad1dec659eff16e
정대현 <eogus_2513@dsm.hs.kr>
2023-05-12 09:27:45 +0900

    fix: CORS 처리

--- src/main/kotlin/com/project/surl/global/configuration/WebConfig.kt
+++ src/main/kotlin/com/project/surl/global/configuration/WebConfig.kt
@@ -5,14 +5,14 @@ import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
-@Configuration
-@EnableWebFlux
+//@Configuration
+//@EnableWebFlux
 class WebConfig : WebFluxConfigurer {
 
-    override fun addCorsMappings(corsRegistry: CorsRegistry) {
-        corsRegistry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .maxAge(3600)
-    }
+//    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+//        corsRegistry.addMapping("/**")
+//            .allowedOrigins("http://localhost:3000")
+//            .allowedMethods("GET", "POST")
+//            .maxAge(3600)
+//    }
 }
