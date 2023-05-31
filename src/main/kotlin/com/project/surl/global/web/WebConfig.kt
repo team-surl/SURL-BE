7aead955bd161e6aedd4d66b9595989b19c460e9
정대현 <eogus_2513@dsm.hs.kr>
2023-05-31 11:17:11 +0900

    add: CORS 설정

--- src/main/kotlin/com/project/surl/global/web/WebConfig.kt
+++ src/main/kotlin/com/project/surl/global/web/WebConfig.kt
@@ -8,9 +8,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 class WebConfig : WebFluxConfigurer {
     override fun addCorsMappings(corsRegistry: CorsRegistry) {
         corsRegistry.addMapping("/**")
-            .allowedMethods("*")
-            .allowCredentials(true)
             .allowedOrigins("http://localhost:3000", "https://surl-fe.vercel.app")
-            .maxAge(3600)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
     }
 }
